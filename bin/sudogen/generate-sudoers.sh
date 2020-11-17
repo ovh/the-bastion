09bd6dffd92b0e9735e4885882fa411348b4d01d
Stéphane Lesimple <speed47_github@speed47.net>
2020-11-17 11:14:34 +0100

    fix: freebsd: add md5sum_compat()
    to account for systems where md5sum's binary name is gmd5sum

--- bin/sudogen/generate-sudoers.sh
+++ bin/sudogen/generate-sudoers.sh
@@ -32,7 +32,7 @@ generate_account_sudoers()
     normalized_account=$(sed -re 's/[^A-Z0-9_]/_/gi' <<< "$account")
     # as we're reducing the amount of possible chars in normalized_account
     # we could have collisions: use MD5 to generate a uniq suffix
-    account_suffix=$(md5sum - <<< "$account" | cut -c1-6)
+    account_suffix=$(md5sum_compat - <<< "$account" | cut -c1-6)
     normalized_account="${normalized_account}_${account_suffix}"
     # lowercase is prohibited
     normalized_account=$(tr '[:lower:]' '[:upper:]' <<< "$normalized_account")
