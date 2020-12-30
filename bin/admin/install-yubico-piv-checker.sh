59d4cc6e4206635c29edb7f66be5d1cae009eccd
Stéphane Lesimple <stephane.lesimple@corp.ovh.com>
2020-12-30 09:56:41 +0000

    feat: add yubico-piv-checker install script

--- bin/admin/install-yubico-piv-checker.sh
+++ bin/admin/install-yubico-piv-checker.sh
@@ -2,7 +2,7 @@
 # vim: set filetype=sh ts=4 sw=4 sts=4 et:
 set -e
 
-PROGRAM_NAME=ovh-ttyrec
+PROGRAM_NAME=yubico-piv-checker
 RELEASE_API_URL="https://api.github.com/repos/ovh/$PROGRAM_NAME/releases"
 
 basedir=$(readlink -f "$(dirname "$0")"/../..)
@@ -23,9 +23,9 @@ action_static() {
     os=$(uname -s | tr '[:upper:]' '[:lower:]')
 
     if [ "$arch" = "x86_64" ] || [ "$arch" = "amd64" ]; then
-        set_download_url "/${PROGRAM_NAME}.*_(x86_|amd)64-$os-static-binary\\.tar\\.gz$"
+        set_download_url "/${PROGRAM_NAME}.*_${os}_(x86_|amd)64\\.tar\\.gz$"
     else
-        set_download_url "/${PROGRAM_NAME}.*_$arch-$os-static-binary\\.tar\\.gz$"
+        set_download_url "/${PROGRAM_NAME}.*_${os}_$arch\\.tar\\.gz$"
     fi
     prepare_temp_folder
 
@@ -33,19 +33,12 @@ action_static() {
     # we have just one archive file in the current temp directory
     # shellcheck disable=SC2035
     tar xzf *.tar.gz
-    # at this point we have just one directory, named ovh-ttyrec-w.x.y.z, just use the shell completion to get in it!
-    cd ovh-ttyrec-*/
     action_done
 
     action_doing "Installing files"
-    for file in ttytime ttyrec ttyplay; do
+    for file in $PROGRAM_NAME; do
         action_detail "/usr/local/bin/$file"
         install -m 0755 "$file" /usr/local/bin/
-    done
-    cd docs
-    for file in *.1; do
-        action_detail "/usr/local/man/man1/$file"
-        install -m 0644 "$file" /usr/local/man/man1/
     done
     action_done
 
