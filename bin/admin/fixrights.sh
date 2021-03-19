6f8e0fb255b4f39a1364a1f1e258a3b9bc893eef
Stéphane Lesimple <stephane.lesimple+bastion@ovhcloud.com>
2021-03-19 15:46:21 +0000

    fix: fixrights.sh: 'chmod --' not supported under FreeBSD

--- bin/admin/fixrights.sh
+++ bin/admin/fixrights.sh
@@ -53,7 +53,7 @@ chmod 0755 "$basedir"/docker/entrypoint.sh \
     "$basedir"/tests/functional/docker/tester_role.sh \
     "$basedir"/tests/functional/fake_ttyrec.sh
 
-find "$basedir"/tests/unit -type f -name "*.pl" -print0 | xargs -r0 chmod 0755 --
+find "$basedir"/tests/unit -type f -name "*.pl" -print0 | xargs -r0 chmod 0755
 
 while IFS= read -r -d '' plugin
 do
