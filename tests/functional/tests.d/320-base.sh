5415ed27931b5de3e66c5efbc0aaa0e11a6facff
thibault.dewailly <thibault.dewailly@corp.ovh.com>
2021-06-25 14:03:19 +0000

    Feat: Add admin and super owner accounts list in info plugin
    For auditing purposes, get admin and super owner list in info plugin
    Available for auditor role only
    Closes #206

--- tests/functional/tests.d/320-base.sh
+++ tests/functional/tests.d/320-base.sh
@@ -7,6 +7,12 @@
 
 testsuite_base()
 {
+    grant accountCreate
+    # create regular account to compare info access between auditor and non auditor
+    success accountCreate a0_create_a1 $a0 --osh accountCreate --always-active --account $account1 --uid $uid1 --public-key "\"$(cat $account1key1file.pub)\""
+    json .error_code OK .command accountCreate .value null
+    revoke accountCreate
+
     # basic stuff and help
     run     base  nocmd     $a0
     retvalshouldbe 112
@@ -30,9 +36,30 @@ testsuite_base()
     contain "Unknown command"
     json .error_code KO_UNKNOWN_COMMAND .command null .value null
 
+    # grant account0 as admin
+    success admin_superowner set_a0_as_admin $r0 "\". $opt_remote_basedir/lib/shell/functions.inc; add_user_to_group_compat $account0 osh-admin\""
+    configchg 's=^\\\\x22adminAccounts\\\\x22.+=\\\\x22adminAccounts\\\\x22:[\\\\x22'"$account0"'\\\\x22],='
+    # grant account1 as auditor
+    success osh   accountGrantAuditor $a0 --osh accountGrantCommand --command auditor --account $account1
+    success osh   info $a1                --osh info
+    contain "Your alias to connect"
+    contain "My admins are: "
+    contain "My super owners are: "
+    json .error_code OK .command info .value.account $account1 .value.adminAccounts '["'"$account0"'"]'
+
+
+    # now check that regular user do not see admins list
     success osh   info $a0 -osh info
     contain "Your alias to connect"
+    nocontain "My admins are: "
+    nocontain "My super owners are: "
     json .error_code OK .command info .value.account $account0
+
+    # delete account1
+    grant accountDelete
+    success admin_superowner delete_a1 $a0 --osh accountDelete --account $account1 --no-confirm
+    revoke accountDelete
+
 }
 
 testsuite_base
