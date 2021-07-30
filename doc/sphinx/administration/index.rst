6971a96b74d8e5a79c8328b69ef279e611567b4b
Stéphane Lesimple <stephane.lesimple+bastion@ovhcloud.com>
2021-07-30 12:00:21 +0000

    doc: generate scripts doc reference

--- doc/sphinx/administration/index.rst
+++ doc/sphinx/administration/index.rst
@@ -4,10 +4,6 @@ Administration
 
 Here we make the assumption that you've already installed The Bastion, and would like to know more about administration topics.
 
-.. warning::
-   The options reference of the satellite configuration files (other than bastion.conf)
-   are not yet documented here, but directly in the configuration files themselves.
-
 .. toctree::
    :maxdepth: 2
 
--- doc/sphinx/build-config-doc-ref.pl
+++ doc/sphinx/build-config-doc-ref.pl
@@ -3,12 +3,16 @@
 use strict;
 use warnings;
 
+use FindBin;
+
 my %h;
 my @out;
 my $section;
 my %sections;
 my %sectiondesc;
 my @orderedsections;
+
+my $document = $ARGV[0] || 'unknown';
 
 sub dumpdoc {
     if (!%h) {
@@ -17,9 +21,12 @@ sub dumpdoc {
     elsif (defined $h{param} && defined $h{default} && defined $h{desc} && defined $h{type}) {
         die "attempting to dump data but section=$section" if !$section;
         push @{$sections{$section}}, $h{param};
-        push @out, ".. _$h{param}:\n\n", "$h{param}\n", "*" x length($h{param}), "\n\n", ":Type: ``$h{type}``\n\n", ":Default: ``$h{default}``\n\n";
-        push @out, ":Example: ``$h{example}``\n\n" if $h{example};
-        push @out, "$h{desc}\n\n";
+        push @out, ".. _$h{param}:\n", "\n"
+          if $ENV{'GLOBAL_REFS'};
+        push @out, "$h{param}\n", "*" x length($h{param}) . "\n", "\n", ":Type: ``$h{type}``\n", "\n", ":Default: ``$h{default}``\n", "\n";
+        push @out, ":Example: ``$h{example}``\n", "\n"
+          if $h{example};
+        push @out, "$h{desc}\n", "\n";
     }
     else {
         die "something is missing: " . ($h{param} ? "" : "param ") . ($h{default} ? "" : "default ") . ($h{desc} ? "" : "desc ") . ($h{type} ? "" : "type") . "\n";
@@ -30,30 +37,42 @@ sub dumpdoc {
 
 my $state = '';
 while (<STDIN>) {
+    $ENV{'DEBUG'} && print;
     next if /^\s*$/;
     if (m{^# ([a-zA-Z0-9_]+) \((.+)\)}) {
         $h{param} = $1;
         $h{type}  = $2;
-        $state    = 'name';
+        $ENV{'DEBUG'} && print "--- state=name\n";
+        $state = 'name';
     }
     elsif (m{^#\s+DESC:\s+(.+)$}) {
         $h{desc} = $1;
+        $ENV{'DEBUG'} && print "--- state=desc\n";
         $state = 'desc';
     }
     elsif (m{^#\s+EXAMPLE:\s+(.+)$}) {
         $h{example} = $1;
+        $ENV{'DEBUG'} && print "--- state=example\n";
         $state = 'example';
     }
     elsif (m{^#\s+DEFAULT:\s+(.+)$}) {
         $h{default} = $1;
+        $ENV{'DEBUG'} && print "--- state=default\n";
         $state = 'default';
     }
     elsif (m{^#\s{0,11}(.*)$} && exists $h{desc} && $state eq 'desc') {
         $h{desc} .= "\n$1";
     }
-    elsif (m{^"([^"]+)"} && $h{param} eq $1) {
+    elsif ((m{^"([^"]+)"} && $h{param} eq $1) || (m{^([A-Za-z0-9_]+)=} && $h{param} eq $1)) {
+        $ENV{'DEBUG'} && print "--- state=param\n";
+        $state = 'param';
+    }
+    elsif (m{^#$} && $state eq 'param') {
+        $ENV{'DEBUG'} && print "--- state=(empty)\n";
         $state = '';
         dumpdoc();
+    }
+    elsif ($state eq 'param') {
     }
     elsif (m{^# > (.+)$}) {
         if (%h or $state) { die "new section '$1' but we have pending data"; }
@@ -66,50 +85,25 @@ while (<STDIN>) {
         $sectiondesc{$section} = $1;
     }
     elsif (!/^##|^[{}]|^#\s*$/) {
-        die("\\--- lost here, state='$state'");
+        die("$_^^^ lost here, state='$state'");
     }
 }
 dumpdoc();
 
-if ($ENV{BASTION_CONF}) {
-    print <<'EOF';
-======================
-bastion.conf reference
-======================
-
-.. note::
-
-   The Bastion has a lot of configuration options so that you can tailor it to your needs.
-   However, if you're just starting and would like to get started quickly, just configure
-   the ``Main Options``. All the other options have sane defaults that can still be customized
-   at a later time.
-
-Option List
-===========
-
-EOF
+if (open(my $fh, "<", "$FindBin::Bin/../sphinx-reference-headers/$document.header")) {
+    local $/;
+    my $contents = <$fh>;
+    close($fh);
+    print $contents;
 }
-elsif ($ENV{OSH_HTTP_PROXY_CONF}) {
-    print <<'EOF';
-=============================
-osh-http-proxy.conf reference
-=============================
-
-.. note::
-
-   This module is optional, and disabled by default. To know more about the HTTP Proxy feature
-   of The Bastion, please check :doc:`/using/http_proxy`
-
-Option List
-===========
-
-EOF
+else {
+    print STDERR "No header found in '$FindBin::Bin/../sphinx-reference-headers/$document.header'\n";
 }
 
 foreach my $section (@orderedsections) {
     die "no description for section $section" if !$sectiondesc{$section};
-    print "\n", $section, "\n", "-" x length($section), "\n\n", $sectiondesc{$section}, "\n\n";
-    print "- :ref:`$_`\n" for @{$sections{$section}};
+    print "\n", "$section options\n", "-" x length("$section options"), "\n\n", $sectiondesc{$section}, "\n\n";
+    print "- `$_`_\n" for @{$sections{$section}};
 }
 print <<'EOF', join('', @out);
 
