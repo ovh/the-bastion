8f88e159c93119bfac825d0c27829b86ba4986f0
Stéphane Lesimple <stephane.lesimple+bastion@ovhcloud.com>
2021-12-22 13:56:05 +0000

    enh: ttyrec & yubico installs: hardcode URLs for when API is down

--- bin/admin/install-ttyrec.sh
+++ bin/admin/install-ttyrec.sh
@@ -52,4 +52,40 @@ action_static() {
     cd /
 }
 
+# only used when/if the API is down and we're in CI mode
+default_urls() {
+    local ver="1.1.6.7"
+    local rpmver="${ver}-1"
+    local list="
+        -${rpmver}.aarch64.rpm
+        -${rpmver}.armv7hnl.rpm
+        -${rpmver}.armv7l.rpm
+        -${rpmver}.i386.rpm
+        -${rpmver}.mips64el.rpm
+        -${rpmver}.ppc64le.rpm
+        -${rpmver}.s390x.rpm
+        -${rpmver}.x86_64.rpm
+        -${ver}_amd64-freebsd-static-binary.tar.gz
+        -${ver}_amd64-linux-static-binary.tar.gz
+        -${ver}_arm64-linux-static-binary.tar.gz
+        -${ver}_armel-linux-static-binary.tar.gz
+        -${ver}_armhf-linux-static-binary.tar.gz
+        -${ver}_i386-linux-static-binary.tar.gz
+        -${ver}_mips64el-linux-static-binary.tar.gz
+        -${ver}_ppc64el-linux-static-binary.tar.gz
+        -${ver}_s390x-linux-static-binary.tar.gz
+        _${ver}_amd64.deb
+        _${ver}_arm64.deb
+        _${ver}_armel.deb
+        _${ver}_armhf.deb
+        _${ver}_i386.deb
+        _${ver}_mips64el.deb
+        _${ver}_ppc64el.deb
+        _${ver}_s390x.deb"
+    for suffix in $list
+    do
+        echo "https://github.com/ovh/ovh-ttyrec/releases/download/v${ver}/ovh-ttyrec${suffix}"
+    done
+}
+
 install_main "$@"
