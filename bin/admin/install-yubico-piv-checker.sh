e847a19857d1511442c3bdd7c8419b35e42aaa95
Stéphane Lesimple <stephane.lesimple+bastion@ovhcloud.com>
2021-12-22 13:56:05 +0000

    enh: ttyrec & yubico installs: hardcode URLs for when API is down

--- bin/admin/install-yubico-piv-checker.sh
+++ bin/admin/install-yubico-piv-checker.sh
@@ -41,4 +41,54 @@ action_static() {
     cd /
 }
 
+# only used when/if the API is down and we're in CI mode
+default_urls() {
+    local ver="1.0.0"
+    local list="
+        -${ver}.aarch64.rpm
+        -${ver}.armv7hnl.rpm
+        -${ver}.armv6l.rpm
+        -${ver}.i386.rpm
+        -${ver}.mips64el.rpm
+        -${ver}.ppc64le.rpm
+        -${ver}.s390x.rpm
+        -${ver}.x86_64.rpm
+        _${ver}_amd64.deb
+        _${ver}_arm64.deb
+        _${ver}_armel.deb
+        _${ver}_armhf.deb
+        _${ver}_darwin_amd64.tar.gz
+        _${ver}_freebsd_386.tar.gz
+        _${ver}_freebsd_amd64.tar.gz
+        _${ver}_freebsd_arm64.tar.gz
+        _${ver}_freebsd_armv5.tar.gz
+        _${ver}_freebsd_armv7.tar.gz
+        _${ver}_i386.deb
+        _${ver}_linux_386.tar.gz
+        _${ver}_linux_amd64.tar.gz
+        _${ver}_linux_arm64.tar.gz
+        _${ver}_linux_armv5.tar.gz
+        _${ver}_linux_armv7.tar.gz
+        _${ver}_linux_mips64le_hardfloat.tar.gz
+        _${ver}_linux_ppc64le.tar.gz
+        _${ver}_linux_s390x.tar.gz
+        _${ver}_mips64el.deb
+        _${ver}_netbsd_386.tar.gz
+        _${ver}_netbsd_amd64.tar.gz
+        _${ver}_netbsd_armv5.tar.gz
+        _${ver}_netbsd_armv7.tar.gz
+        _${ver}_openbsd_386.tar.gz
+        _${ver}_openbsd_amd64.tar.gz
+        _${ver}_openbsd_arm64.tar.gz
+        _${ver}_openbsd_armv5.tar.gz
+        _${ver}_openbsd_armv7.tar.gz
+        _${ver}_ppc64le.deb
+        _${ver}_s390x.deb
+        _${ver}_windows_amd64.tar.gz"
+    for suffix in $list
+    do
+        echo "https://github.com/ovh/yubico-piv-checker/releases/download/v${ver}/yubico-piv-checker${suffix}"
+    done
+}
+
 install_main "$@"
