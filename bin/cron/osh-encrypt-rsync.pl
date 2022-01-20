2c2064a4841fbb96972707c142d4902e623df354
Stéphane Lesimple <stephane.lesimple+bastion@ovhcloud.com>
2022-01-20 15:51:11 +0000

    feat: osh-encrypt-rsync: handle sqlite and user logs along with ttyrec files

--- bin/cron/osh-encrypt-rsync.pl
+++ bin/cron/osh-encrypt-rsync.pl
@@ -5,10 +5,9 @@ use warnings;
 use 5.010;
 
 use File::Temp;
-use File::Basename;
 use File::Find;
 use File::Path;
-use File::Copy;
+use File::Copy 'move';
 use Getopt::Long;
 use Fcntl qw{ :flock };
 use IPC::Open3;
@@ -20,7 +19,8 @@ use OVH::Bastion;
 use OVH::SimpleLog;
 
 my %config;
-my ($dryRun, $configTest, $forceRsync, $forceEncrypt, $noDelete, $encryptOnly, $rsyncOnly, $verbose, $help);
+my ($dryRun, $configTest, $forceDelete, $forceEncrypt, $noDelete, $encryptOnly, $rsyncOnly, $verbose, $help);
+$verbose = 0;
 local $| = 1;
 
 sub is_new_gpg {
@@ -41,7 +41,7 @@ sub gpg_sign {
     my %params = @_;
     my @cmd    = qw{ gpg --batch --trust-model always --sign --passphrase-fd 0 };
     push @cmd, qw{ --pinentry-mode loopback } if is_new_gpg();
-    push @cmd, "-v" if $config{'trace'};
+    push @cmd, "-v" if $verbose >= 2;
     push @cmd, '--local-user', $params{'signkey'}, '--output', '-', $params{'infile'};
 
     my $outfile;
@@ -78,7 +78,7 @@ sub gpg_encrypt {
         push @cmd, qw{ --pinentry-mode loopback } if is_new_gpg();
         push @cmd, '--local-user', $params{'signkey'};
     }
-    push @cmd, "-v" if $config{'trace'};
+    push @cmd, "-v" if $verbose >= 2;
     foreach my $recipient (@{$params{'recipients'}}) {
         push @cmd, "-r", $recipient;
     }
@@ -103,12 +103,66 @@ sub gpg_encrypt {
     return 0;    # success
 }
 
-sub test_config {
-    my $error;
+sub config_load_and_lint {
+    my $fnret;
+
+    # Useful when erroring before we had a chance to actually read the config,
+    # and the configured syslog_facility value. This will be overriden below once we
+    # know what the user configured.
+    OVH::SimpleLog::setSyslog('local6');
+
+    # we can have CONFIGDIR/osh-encrypt-rsync.conf
+    # but also CONFIGDIR/osh-encrypt-rsync.conf.d/*
+    # later files override the previous ones, item by item
+    my @configfilelist;
+    if (-f -r OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf") {
+        push @configfilelist, OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf";
+    }
+
+    if (-d -x OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf.d") {
+        if (opendir(my $dh, OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf.d")) {
+            my @subfiles = map { OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf.d/" . $_ } grep { /\.conf$/ } readdir($dh);
+            closedir($dh);
+            push @configfilelist, sort @subfiles;
+        }
+    }
+
+    # no config file, fail early
+    if (not @configfilelist) {
+        _err "Error, no config file found!";
+        return 1;
+    }
+
+    # load config files in order
+    foreach my $configfile (@configfilelist) {
+        _log "Configuration: loading configfile $configfile...";
+        $fnret = OVH::Bastion::load_configuration_file(
+            file     => $configfile,
+            rootonly => 1,
+        );
+        if (not $fnret) {
+            _err "Error while loading configuration from $configfile, aborting (" . $fnret->msg . ")";
+            return 1;
+        }
+        foreach my $key (keys %{$fnret->value}) {
+            $config{$key} = $fnret->value->{$key};
+        }
+
+        # we'll be using our own config file as a handy flock() backend
+        $config{'lockfile'} = $configfile if not defined $config{'lockfile'};
+    }
+
+    # set logging info as soon as we can, before vetting the rest of the config
+    $config{'syslog_facility'} //= 'local6';
+    if ($config{'syslog_facility'}) {
+        OVH::SimpleLog::setSyslog($config{'syslog_facility'});
+    }
+    else {
+        OVH::SimpleLog::closeSyslog();
+    }
+    OVH::SimpleLog::setLogFile($config{'logfile'}) if $config{'logfile'};
 
     # normalize / define defaults / quick checks
-    $config{'trace'} = $config{'trace'} ? 1 : 0;
-
     if (not exists $config{'recipients'}) {
         _err "config error: recipients must be defined";
         return 1;
@@ -123,17 +177,86 @@ sub test_config {
         return 1;
     }
 
-    if ($config{'encrypt_and_move_delay_days'} !~ /^\d+$/) {
-        _err "config error: encrypt_and_move_delay_days is not a positive integer!";
+    $config{'encrypt_and_move_to_directory'} //= '/home/.encrypt';
+
+    # new config option found
+    if (defined $config{'encrypt_and_move_ttyrec_delay_days'}) {
+
+        # check proper syntax
+        if ($config{'encrypt_and_move_ttyrec_delay_days'} !~ /^(?:\d+|-1)$/) {
+            _err "config error: encrypt_and_move_ttyrec_delay_days is not a positive integer nor -1!";
+            return 1;
+        }
+
+        # syntax is good but we also have the deprecated name, warn and proceed
+        if (defined $config{'encrypt_and_move_delay_days'}) {
+            _warn "config: deprecated option 'encrypt_and_move_delay_days' exists, but has been ignored as "
+              . "we also have the new option 'encrypt_and_move_ttyrec_delay_days' in the configuration";
+        }
+    }
+
+    # new config option not found
+    else {
+        # do we have the legacy option name ?
+        if (defined $config{'encrypt_and_move_delay_days'}) {
+
+            # yes, check proper syntax
+            if ($config{'encrypt_and_move_delay_days'} !~ /^(?:\d+|-1)$/) {
+                _err "config error: encrypt_and_move_delay_days is not an integer >= -1!";
+                return 1;
+            }
+            else {
+                # syntax ok, save it to the new name
+                $config{'encrypt_and_move_ttyrec_delay_days'} = delete $config{'encrypt_and_move_delay_days'};
+            }
+        }
+    }
+
+    foreach my $key (qw{ encrypt_and_move_user_logs_delay_days encrypt_and_move_user_sqlites_delay_days }) {
+        $config{$key} //= 31;
+        if ($config{$key} !~ /^(?:\d+|-1)$/) {
+            _err "config error: $key is not an integer >= -1!";
+            return 1;
+        }
+    }
+
+    $config{'rsync_delay_before_remove_days'} //= 0;
+    if ($config{'rsync_delay_before_remove_days'} !~ /^(?:\d+|-1)$/) {
+        _err "config error: rsync_delay_before_remove_days is not an integer >= -1!";
         return 1;
     }
 
-    if ($config{'rsync_delay_before_remove_days'} !~ /^\d+$/) {
-        _err "config error: rsync_delay_before_remove_days is not a positive integer!";
+    $config{'rsync_destination'} //= '';
+    $config{'rsync_rsh'}         //= '';
+
+    # ensure the various config files defined all the keywords we need
+    foreach my $keyword (qw{ signing_key signing_key_passphrase }) {
+        next if defined $config{$keyword};
+        _err "Missing mandatory configuration item '$keyword', aborting";
         return 1;
     }
 
-    # ok, check if my gpg conf is good
+    _log "Config successfully loaded.";
+
+    if ($verbose) {
+        require Data::Dumper;
+        local $Data::Dumper::Sortkeys = 1;
+        local $Data::Dumper::Terse    = 1;
+
+        # hide passphrase
+        my $passphrase = $config{'signing_key_passphrase'};
+        $config{'signing_key_passphrase'} = '***REDACTED***';
+        print Data::Dumper::Dumper({config => \%config});
+        $config{'signing_key_passphrase'} = $passphrase;
+    }
+
+    return 0;
+}
+
+sub config_test {
+    my $error;
+
+    # check if my gpg conf is good
     my $infile = File::Temp->new(UNLINK => 1, TMPDIR => 1);
     print {$infile} time();
     close($infile);
@@ -293,21 +416,53 @@ sub encrypt_once {
     return 0;    # no error
 }
 
-my $openedFiles = undef;
-
+# this sub is called for each file found
 sub potentially_work_on_this_file {
 
-    # file must be a ttyrec file or an osh_http_proxy_ttyrec-ish file
-    my $filetype;
-    $filetype = 'ttyrec'   if m{^/home/[^/]+/ttyrec/[^/]+/[^/]+(\.ttyrec(\.zst)?)?$};
-    $filetype = 'proxylog' if m{^/home/[^/]+/ttyrec/[^/]+/\d+-\d+-\d+\.txt$};
-    $filetype or return;
+    # file must be either:
+    # - a ttyrec file or an osh_http_proxy_ttyrec-ish file
+    # - a user sqlite file (possibly compressed)
+    # - a user log file (possibly compressed)
+    my ($filetype, $file_delay);
+    if (m{^/home/[^/]+/ttyrec/[^/]+/[^/]+(?:\.ttyrec(?:\.zst)?)?$}) {
+        $filetype   = 'ttyrec';
+        $file_delay = $config{'encrypt_and_move_ttyrec_delay_days'};
+    }
+    elsif (m{^/home/[^/]+/ttyrec/[^/]+/\d+-\d+-\d+\.txt$}) {
+        $filetype   = 'proxylog';
+        $file_delay = $config{'encrypt_and_move_ttyrec_delay_days'};
 
-    # must exist and be a file
+        # never touch a file that's too recent because we might still write to it:
+        $file_delay = 1 if $file_delay < 1;
+    }
+    elsif (m{^/home/[^/]+/[^/]+\.log(?:\.gz|\.xz)?$}) {
+        $filetype   = 'userlog';
+        $file_delay = $config{'encrypt_and_move_user_logs_delay_days'};
+
+        # never touch a file that's too recent because we might still write to it:
+        $file_delay = 31 if $file_delay < 31;
+    }
+    elsif (m{^/home/[^/]+/[^/]+\.sqlite(?:\.gz|\.xz)?$}) {
+        $filetype   = 'usersqlite';
+        $file_delay = $config{'encrypt_and_move_user_sqlites_delay_days'};
+
+        # never touch a file that's too recent because we might still write to it:
+        $file_delay = 31 if $file_delay < 31;
+    }
+    else {
+        # ignore this file
+        return;
+    }
+
+    # we might not have the right to touch some filetypes, as per config
+    return if ($file_delay < 0);
+
+    # $_ must exist and be a file
     -f or return;
     my $file = $_;
 
-    # first, check if we populated $openedFiles as a hashref
+    # first, populate (once) the list of ttyrec files that are still opened by ttyrec
+    state $openedFiles;
     if (ref $openedFiles ne 'HASH') {
         $openedFiles = {};
         if (open(my $fh_lsof, '-|', "lsof -a -n -c ttyrec -- /home/")) {
@@ -323,25 +478,20 @@ sub potentially_work_on_this_file {
         }
     }
 
-    # still open ? don't touch
+    # still open? don't touch
     if (exists $openedFiles->{$file}) {
         _log "File $file is still opened by ttyrec, skipping";
         return;
     }
 
-    # and must be older than encrypt_and_move_delay_days days
+    # ignore files that are too recent (as per config)
     my $mtime = (stat($file))[9];
-    if ($mtime > time() - 86400 * $config{'encrypt_and_move_delay_days'}) {
-        _log "File $file is too recent, skipping" if $verbose;
+    if ($mtime > time() - 86400 * $file_delay) {
+        _log "File $file is too recent ($filetype: $file_delay days), skipping" if $verbose;
         return;
     }
 
-    # for proxylog, never touch a file that's < 86400 sec old (because we might still write to it)
-    if ($filetype eq 'proxylog' and $mtime > time() - 86400) {
-        _log "File $file is too recent (proxylog), skipping" if $verbose;
-        return;
-    }
-
+    # ok, this file is eligible, go
     my $error = encrypt_multi(source_file => $file, destination_directory => $config{'encrypt_and_move_to_directory'}, remove_source_on_success => not $noDelete);
     if ($error) {
         _err "Got an error for $file, skipping!";
@@ -356,6 +506,7 @@ sub directory_filter {    ## no critic (RequireArgUnpacking)
     if ($File::Find::dir eq '/home') {
         my @out = ();
         foreach (@_) {
+            push @out, $_ if -e "/home/$_/lastlog";
             if (-d "/home/$_/ttyrec") {
                 push @out, $_ if -d "/home/$_/ttyrec";
             }
@@ -376,17 +527,20 @@ sub print_usage {
     $0 [options]
 
     --dry-run        Don't actually compress/encrypt/rsync, just show what would be done
-    --config-test    Test the validity of the config file
-    --verbose        More logs
-
-    rsync phase:
-    --no-delete      During the encrypt phase, don't delete the source files after successful encryption
-    --rsync-only     Skip the encrypt phase, just rsync the already encrypted & moved files
-    --force-rsync    Don't wait for the configured number of days before rsyncing files, do it immediately
+    --config-test    Test the validity of the config file and GPG setup
+    --verbose        More logs, use twice to also get gpg raw output
 
     encryption phase:
-    --encrypt-only   Skip the rsync phase, just encrypt & move the files
-    --force-encrypt  Don't wait for the configured number of days before encrypting & moving files, do it immediately
+    --encrypt-only   Encrypt and move the files, but skip the rsync phase
+    --force-encrypt  Don't wait for the configured number of days before encrypting & moving files, do it immediately.
+                     Note that filetypes that have their amount of days set to -1 from the config file will still be ignored,
+                     and the minimum configurable amount of time still applies per filetype (i.e. to avoid moving a file still in use).
+
+    rsync phase:
+    --rsync-only     Skip the encryption phase, just rsync the already encrypted & moved files
+    --force-delete   Don't wait for the configured number of days before deleting rsynced files,
+                     do it as soon as they're transferred
+    --no-delete      Don't delete local files after rsyncing, even if the configured amount of days has passed
 
 EOF
     return;
@@ -402,9 +556,9 @@ sub main {
             "no-delete"     => \$noDelete,
             "encrypt-only"  => \$encryptOnly,
             "rsync-only"    => \$rsyncOnly,
-            "force-rsync"   => \$forceRsync,
+            "force-delete"  => \$forceDelete,
             "force-encrypt" => \$forceEncrypt,
-            "verbose"       => \$verbose,
+            "verbose+"      => \$verbose,
             "help"          => \$help,
         )
       )
@@ -419,55 +573,16 @@ sub main {
         return 0;
     }
 
-    # we can have CONFIGDIR/osh-encrypt-rsync.conf
-    # but also CONFIGDIR/osh-encrypt-rsync.conf.d/*
-    # later files override the previous ones, item by item
-
-    my $fnret;
-    my $lockfile;
-    my @configfilelist;
-    if (-f -r OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf") {
-        push @configfilelist, OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf";
-    }
-
-    if (-d -x OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf.d") {
-        if (opendir(my $dh, OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf.d")) {
-            my @subfiles = map { OVH::Bastion::main_configuration_directory() . "/osh-encrypt-rsync.conf.d/" . $_ } grep { /\.conf$/ } readdir($dh);
-            closedir($dh);
-            push @configfilelist, sort @subfiles;
-        }
-    }
-
-    if (not @configfilelist) {
-        _err "Error, no config file found!";
+    if (config_load_and_lint() != 0) {
+        _err "Configuration is invalid, aborting";
         return 1;
     }
-
-    foreach my $configfile (@configfilelist) {
-        _log "Configuration: loading configfile $configfile...";
-        $fnret = OVH::Bastion::load_configuration_file(
-            file     => $configfile,
-            rootonly => 1,
-        );
-        if (not $fnret) {
-            _err "Error while loading configuration from $configfile, aborting (" . $fnret->msg . ")";
-            return 1;
-        }
-        foreach my $key (keys %{$fnret->value}) {
-            $config{$key} = $fnret->value->{$key};
-        }
-
-        # we'll be using our own config file as a handy flock() backend
-        $lockfile = $configfile if not defined $lockfile;
-    }
-
-    $verbose ||= $config{'verbose'};
 
     # ensure no other copy of myself is already running
     # except if we are in rsync-only mode (concurrency is then not a problem)
     my $lockfh;
     if (not $rsyncOnly) {
-        if (!open($lockfh, '<', $lockfile)) {
+        if (!open($lockfh, '<', $config{'lockfile'})) {
 
             # flock() needs a file handler
             _log "Couldn't open config file, aborting";
@@ -479,33 +594,20 @@ sub main {
         }
     }
 
-    # default values
-    $config{'syslog_facility'} ||= 'local6';
-
-    # ensure the various config files defined all the keywords we need
-    foreach my $keyword (
-        qw{
-        signing_key signing_key_passphrase recipients encrypt_and_move_to_directory
-        encrypt_and_move_delay_days rsync_delay_before_remove_days
-        }
-      )
-    {
-        next if defined $config{$keyword};
-        _err "Missing mandatory configuration item '$keyword', aborting";
-        return 1;
-    }
-
-    OVH::SimpleLog::setLogFile($config{'logfile'})        if $config{'logfile'};
-    OVH::SimpleLog::setSyslog($config{'syslog_facility'}) if $config{'syslog_facility'};
-
-    if ($forceRsync) {
-        $config{'rsync_delay_days'} = 0;
+    if ($forceDelete) {
+        $config{'rsync_delay_before_remove_days'} = 0;
     }
     if ($forceEncrypt) {
-        $config{'encrypt_and_move_delay_days'} = 0;
+        foreach my $type (qw{ ttyrec user_logs useer_sqlites }) {
+
+            # keep config at -1 if it's set at -1 (i.e. filetype disabled)
+            $config{"encrypt_and_move_${type}_delay_days"} = 0 if $config{"encrypt_and_move_${type}_delay_days"} > 0;
+        }
     }
 
-    if (test_config() != 0) {
+    $verbose ||= $config{'verbose'};
+
+    if (config_test() != 0) {
         _err "Config test failed, aborting";
         return 1;
     }
@@ -600,7 +702,7 @@ sub main {
         }
     }
 
-    _log "Done";
+    _log "Done, got " . (OVH::SimpleLog::nb_errors()) . " error(s) and " . (OVH::SimpleLog::nb_warnings()) . " warning(s).";
     return 0;
 }
 
