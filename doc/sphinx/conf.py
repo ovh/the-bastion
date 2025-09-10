735c0314d3315e9671108a2d6e857495e19d8155
Stéphane Lesimple <stephane.lesimple+bastion@ovhcloud.com>
2025-09-10 12:02:00 +0000

    doc: note about the refresh of bastion.conf (#573)

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -70,7 +70,7 @@ highlight_language = 'shell'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+# language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
