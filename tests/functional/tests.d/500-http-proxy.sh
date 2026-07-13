73072aa17880642397b3d4819f80685b8f8a0a96
Stéphane Lesimple <stephane.lesimple+bastion@ovhcloud.com>
2026-07-13 12:48:42 +0000

    enh: enable FreeBSD tests for HTTP Proxy and jumphost

--- tests/functional/tests.d/500-http-proxy.sh
+++ tests/functional/tests.d/500-http-proxy.sh
@@ -28,12 +28,12 @@ testsuite_proxy()
     # as a --no-color or similar option doesn't seem to exist for curl.
 
     # check that the proxy is up
-    script monitoring "curl -m $default_timeout -ski https://$remote_ip:$remote_proxy_port/bastion-health-check | cat; exit \${PIPESTATUS[0]}"
+    script monitoring "curl -m $default_timeout -vki https://$remote_ip:$remote_proxy_port/bastion-health-check | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'running nominally'
 
     # and let's go
-    script noauth "curl -m $default_timeout -ski https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script noauth "curl -m $default_timeout -vki https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     testsuite_proxy_check_headers
     contain 'HTTP/1.0 401 Authorization required (no auth provided)'
@@ -41,7 +41,7 @@ testsuite_proxy()
     contain 'Content-Type: text/plain'
     contain 'No authentication provided, and authentication is mandatory'
 
-    script bad_auth_format "curl -m $default_timeout -ski -u test:test https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script bad_auth_format "curl -m $default_timeout -vki -u test:test https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 400 Bad Request (bad login format)'
     testsuite_proxy_check_headers
@@ -49,7 +49,7 @@ testsuite_proxy()
     contain 'Content-Type: text/plain'
     contain 'Expected an Authorization line with credentials of the form'
 
-    script bad_auth "curl -m $default_timeout -ski -u test@test@test:test https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script bad_auth "curl -m $default_timeout -vki -u test@test@test:test https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 403 Access Denied'
     contain 'Server: The Bastion'
@@ -66,7 +66,7 @@ testsuite_proxy()
     proxy_password=$(get_json | jq -r '.value.password')
 
     # now try to use these
-    script good_auth_bad_host "curl -m $default_timeout -ski -u '$account0@test@test.invalid:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script good_auth_bad_host "curl -m $default_timeout -vki -u '$account0@test@test.invalid:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 400 Bad Request (ERR_HOST_NOT_FOUND)'
     testsuite_proxy_check_headers
@@ -77,7 +77,7 @@ testsuite_proxy()
     contain 'X-Bastion-Request-Length: 0'
     contain 'X-Bastion-Local-Status: 400'
     contain 'Content-Type: text/plain'
-    contain "Unable to resolve 'test.invalid' (Name or service not known)"
+    contain "Unable to resolve 'test.invalid' ("
 
     # change credentials again
     success generate_proxy_password2 $a0 --osh selfGenerateProxyPassword --do-it
@@ -86,7 +86,7 @@ testsuite_proxy()
     proxy_password2=$(get_json | jq -r '.value.password')
 
     # attempt to use the previous credentials (and fail)
-    script bad_auth2 "curl -m $default_timeout -ski -u test@test@test:test https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script bad_auth2 "curl -m $default_timeout -vki -u test@test@test:test https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     testsuite_proxy_check_headers
     contain 'HTTP/1.0 403 Access Denied'
@@ -99,7 +99,7 @@ testsuite_proxy()
     # a remote user name with forbidden characters must be rejected (regression test for the
     # user revalidation done by both the daemon and the worker, see is_valid_remote_user(stricter => 1)).
     # '!' is rejected under stricter mode; the daemon's previous (loose, unanchored) regex let it through.
-    script bad_user_name "curl -m $default_timeout -ski -u '$account0@te!st@127.0.0.1:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script bad_user_name "curl -m $default_timeout -vki -u '$account0@te!st@127.0.0.1:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 400 Bad Request (bad user name)'
     testsuite_proxy_check_headers
@@ -107,7 +107,7 @@ testsuite_proxy()
     contain 'Content-Type: text/plain'
     contain "User name 'te!st' has forbidden characters"
 
-    script good_auth_no_access "curl -m $default_timeout -ski -u '$account0@test@127.0.0.1:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script good_auth_no_access "curl -m $default_timeout -vki -u '$account0@test@127.0.0.1:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 403 Access Denied (access denied to remote)'
     testsuite_proxy_check_headers
@@ -120,7 +120,7 @@ testsuite_proxy()
     contain 'Content-Type: text/plain'
     contain "This account doesn't have access to this user@host tuple (Access denied for $account0 to test@127.0.0.1:443)"
 
-    script good_auth_no_access_other_port "curl -m $default_timeout -ski -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script good_auth_no_access_other_port "curl -m $default_timeout -vki -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 403 Access Denied (access denied to remote)'
     testsuite_proxy_check_headers
@@ -137,7 +137,7 @@ testsuite_proxy()
     success add_personal_access $a0 --osh selfAddPersonalAccess --host 127.0.0.1 --port-any --user test --force
     json .command selfAddPersonalAccess .error_code OK
 
-    script missing_egress_pwd "curl -m $default_timeout -ski -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script missing_egress_pwd "curl -m $default_timeout -vki -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 412 Precondition Failed (egress password missing)'
     testsuite_proxy_check_headers
@@ -155,7 +155,7 @@ testsuite_proxy()
     json .command selfGeneratePassword .error_code OK .value.account $account0 .value.context account
 
     # and retry
-    script bad_certificate "curl -m $default_timeout -ski -H 'X-Bastion-Enforce-Secure: 1' -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script bad_certificate "curl -m $default_timeout -vki -H 'X-Bastion-Enforce-Secure: 1' -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     # not all versions of LWP add "(certificate verify failed)" at the end of the below error message, so omit it
     contain "HTTP/1.0 500 Can't connect to 127.0.0.1:9443"
@@ -169,7 +169,7 @@ testsuite_proxy()
     contain 'Content-Type: text/plain'
     contain "Can't connect to 127.0.0.1:9443"
 
-    script insecure "curl -m $default_timeout -ski -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script insecure "curl -m $default_timeout -vki -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain "HTTP/1.0 200 OK"
     testsuite_proxy_check_headers
@@ -188,7 +188,7 @@ testsuite_proxy()
     contain "Content-Length: 64"
 
     # generate 1MB of data
-    script one_megabyte "curl -m $default_timeout -ski -H 'X-Test-Add-Response-Header-Content-Type: application/json' -H 'X-Test-Wanted-Response-Size: 1000000' -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script one_megabyte "curl -m $default_timeout -vki -H 'X-Test-Add-Response-Header-Content-Type: application/json' -H 'X-Test-Wanted-Response-Size: 1000000' -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain "HTTP/1.0 200 OK"
     testsuite_proxy_check_headers
@@ -215,7 +215,7 @@ testsuite_proxy()
     script graceful_reload "
         out=\$(mktemp)
         # 1) start a slow request: the fake remote holds the response for 8s, keeping it in-flight
-        curl -ski --max-time 40 -H 'X-Test-Response-Delay: 8' -H 'X-Test-Wanted-Response-Size: 54321' -u '$account0@test@127.0.0.1%9443:$proxy_password' 'https://$remote_ip:$remote_proxy_port/test' > \"\$out\" 2>&1 &
+        curl -vki --max-time 40 -H 'X-Test-Response-Delay: 8' -H 'X-Test-Wanted-Response-Size: 54321' -u '$account0@test@127.0.0.1%9443:$proxy_password' 'https://$remote_ip:$remote_proxy_port/test' > \"\$out\" 2>&1 &
         slowpid=\$!
         # 2) give it time to be accepted and connected to the slow remote
         sleep 3
@@ -227,7 +227,7 @@ testsuite_proxy()
         echo BEGIN_NEW_REQUEST
         # pipe through cat so curl's stdout isn't a TTY: recent curl colorizes header names
         # with ANSI codes otherwise, which would break the literal Content-Length match below
-        curl -ski --max-time 15 -u '$account0@test@127.0.0.1%9443:$proxy_password' 'https://$remote_ip:$remote_proxy_port/test' | cat
+        curl -vki --max-time 15 -u '$account0@test@127.0.0.1%9443:$proxy_password' 'https://$remote_ip:$remote_proxy_port/test' | cat
         echo END_NEW_REQUEST
         # 5) the in-flight request must have completed with its full body, not been cut off
         wait \$slowpid
@@ -248,12 +248,12 @@ testsuite_proxy()
     nocontain 'Empty reply from server'
 
     # the daemon must still be healthy after the in-place reload
-    script post_reload_health "curl -m $default_timeout -ski https://$remote_ip:$remote_proxy_port/bastion-health-check | cat; exit \${PIPESTATUS[0]}"
+    script post_reload_health "curl -m $default_timeout -vki https://$remote_ip:$remote_proxy_port/bastion-health-check | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'running nominally'
 
     # use a disallowed method
-    script forbidden_method "curl -m $default_timeout -ski -X PUT -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script forbidden_method "curl -m $default_timeout -vki -X PUT -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 400 Bad Request (method forbidden)'
     contain 'Server: The Bastion'
@@ -264,16 +264,22 @@ testsuite_proxy()
     contain 'is forbidden by policy'
 
     # use alternate config to only allow more methods
-    success config_swap $r0 "\"cp /etc/bastion/osh-http-proxy-methods.conf /etc/bastion/osh-http-proxy.conf\""
+    success config_swap $r0 "\"cp $opt_remote_etc_bastion/osh-http-proxy-methods.conf $opt_remote_etc_bastion/osh-http-proxy.conf\""
 
     # when daemon will restart, it'll log stuff, ignore it
     ignorecodewarn 'osh-http-proxy-daemon'
-    # pkill doesn't work well under FreeBSD, so do it ourselves for all OSes
-    success force_restart $r0 "\"ps -U proxyhttp -o pid,command | grep -v PID | awk '{print \\\$1}' | xargs -r kill; true\""
-    waitfor 4 "waiting for daemon restart"
+    # pkill doesn't work well under FreeBSD, so do it ourselves for all OSes. loop with SIGKILL
+    # until no proxyhttp-owned process remains: a plain SIGTERM lets the daemon shut down
+    # gracefully and, on FreeBSD, keep the listen socket bound for several seconds, while any
+    # children respawned during the kill race would survive a one-shot snapshot kill. only once
+    # the old tree is fully reaped can target_role.sh's watchdog bring up a fresh daemon.
+    success force_restart $r0 "\"while ps -U proxyhttp -o pid= | grep -q '[0-9]'; do ps -U proxyhttp -o pid= | xargs -r kill -KILL 2>/dev/null; sleep 0.2; done; true\""
+    # then actively wait for the freshly-restarted daemon to actually answer (the old one is
+    # guaranteed gone above, so a successful health-check means the new daemon is serving)
+    wait_for_proxy_up "waiting for daemon restart"
 
     # post some data
-    script post_data "curl -m $default_timeout -ski -X POST -d somedata -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script post_data "curl -m $default_timeout -vki -X POST -d somedata -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain "HTTP/1.0 200 OK"
     testsuite_proxy_check_headers
@@ -293,7 +299,7 @@ testsuite_proxy()
     contain "somedata"
 
     # put some data
-    script patch_data "curl -m $default_timeout -ski -X PUT -d somedata -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script patch_data "curl -m $default_timeout -vki -X PUT -d somedata -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain "HTTP/1.0 200 OK"
     testsuite_proxy_check_headers
@@ -313,7 +319,7 @@ testsuite_proxy()
     contain "somedata"
 
     # put some data with no body
-    script patch_data_no_body "curl -m $default_timeout -ski -X PUT -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script patch_data_no_body "curl -m $default_timeout -vki -X PUT -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain "HTTP/1.0 200 OK"
     testsuite_proxy_check_headers
@@ -332,7 +338,7 @@ testsuite_proxy()
     contain "Content-Length: 64"
 
     # use a disallowed egress method
-    script forbidden_egress_protocol "curl -m $default_timeout -ski -H 'X-Bastion-Egress-Protocol: http' -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script forbidden_egress_protocol "curl -m $default_timeout -vki -H 'X-Bastion-Egress-Protocol: http' -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 400 Bad Request (forbidden egress protocol)'
     testsuite_proxy_check_headers
@@ -341,18 +347,19 @@ testsuite_proxy()
     contain 'not allowed by policy'
 
     # use alternate config to only allow http egress
-    success config_swap $r0 "\"cp /etc/bastion/osh-http-proxy-httponly.conf /etc/bastion/osh-http-proxy.conf\""
+    success config_swap $r0 "\"cp $opt_remote_etc_bastion/osh-http-proxy-httponly.conf $opt_remote_etc_bastion/osh-http-proxy.conf\""
 
     # when daemon will restart, it'll log stuff, ignore it
     ignorecodewarn 'osh-http-proxy-daemon'
-    # pkill doesn't work well under FreeBSD, so do it ourselves for all OSes
-    success force_restart $r0 "\"ps -U proxyhttp -o pid,command | grep -v PID | awk '{print \\\$1}' | xargs -r kill; true\""
-    waitfor 4 "waiting for daemon restart"
+    # pkill doesn't work well under FreeBSD, so do it ourselves for all OSes (see above for the
+    # rationale behind the SIGKILL loop and the active readiness poll)
+    success force_restart $r0 "\"while ps -U proxyhttp -o pid= | grep -q '[0-9]'; do ps -U proxyhttp -o pid= | xargs -r kill -KILL 2>/dev/null; sleep 0.2; done; true\""
+    wait_for_proxy_up "waiting for daemon restart"
 
     # when daemon will restart, it'll log stuff, ignore it
     ignorecodewarn 'osh-http-proxy-daemon'
     # http should be allowed now
-    script allowed_http_egress "curl -m $default_timeout -ski -H 'X-Bastion-Egress-Protocol: http' -u '$account0@test@127.0.0.1%22:$proxy_password' https://$remote_ip:$remote_proxy_port/test 2>&1 | cat; exit \${PIPESTATUS[0]}"
+    script allowed_http_egress "curl -m $default_timeout -vki -H 'X-Bastion-Egress-Protocol: http' -u '$account0@test@127.0.0.1%22:$proxy_password' https://$remote_ip:$remote_proxy_port/test 2>&1 | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 200'
     testsuite_proxy_check_headers
@@ -364,7 +371,7 @@ testsuite_proxy()
     contain 'SSH-2.0'
 
     # take this opportunity to test the remote-host header (otherwise, call is the same as above)
-    script allowed_http_egress_test_host "curl -m $default_timeout -ski -H 'X-Bastion-Egress-Protocol: http' -u '$account0@test@localhost%22:$proxy_password' https://$remote_ip:$remote_proxy_port/test 2>&1 | cat; exit \${PIPESTATUS[0]}"
+    script allowed_http_egress_test_host "curl -m $default_timeout -vki -H 'X-Bastion-Egress-Protocol: http' -u '$account0@test@localhost%22:$proxy_password' https://$remote_ip:$remote_proxy_port/test 2>&1 | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 200'
     testsuite_proxy_check_headers
@@ -376,7 +383,7 @@ testsuite_proxy()
     contain 'SSH-2.0'
 
     # and https disallowed
-    script forbidden_https_egress "curl -m $default_timeout -ski -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script forbidden_https_egress "curl -m $default_timeout -vki -u '$account0@test@127.0.0.1%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'HTTP/1.0 400 Bad Request (forbidden egress protocol)'
     testsuite_proxy_check_headers
@@ -385,7 +392,7 @@ testsuite_proxy()
     contain 'not allowed by policy'
 
     # try an IPv6
-    script ipv6 "curl -m $default_timeout -ski -H 'X-Bastion-Egress-Protocol: http' -u '$account0@test@[::1]%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
+    script ipv6 "curl -m $default_timeout -vki -H 'X-Bastion-Egress-Protocol: http' -u '$account0@test@[::1]%9443:$proxy_password' https://$remote_ip:$remote_proxy_port/test | cat; exit \${PIPESTATUS[0]}"
     retvalshouldbe 0
     contain 'ERR_IP_VERSION_DISABLED'
 }
